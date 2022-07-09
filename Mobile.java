af188f35fe059318c114944cc6b91d9cdf924006
Ajith-Johnson <108991735+Ajith-Johnson@users.noreply.github.com>
2022-07-09 21:37:13 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Mobile {
+	public void makecall() {
+		System.out.println("Make a phone call"); // Creating an method functions
+
+	}
+
+	public void sendMsg() {
+		System.out.println("Send the message"); // Creating an method functions
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("This is my mobile");
+	}
+
+}
